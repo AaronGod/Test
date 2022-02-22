6455d8d03bcaa02e9206765f9c8a6be3358cd15b
aaronpeng <aaronpeng.gan@foxmail.com>
2022-02-22 15:25:38 +0800

    修改了一系列文件

--- useMomentFormatTime.js
+++ useMomentFormatTime.js
@@ -1,7 +1,13 @@
 // 导入需要的包
 const moment = require('moment')
+    // import { moment } from "moment"
 
 // console.log(moment)
 const dt = moment().format('YYYY-MM-DD HH:mm:ss')
 
 console.log(dt)
+    // (function() {
+    //     setInterval(() => {
+    //         console.log(dt)
+    //     }, 1000)
+    // }())
